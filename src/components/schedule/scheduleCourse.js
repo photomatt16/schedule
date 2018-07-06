8677c08ddc1061680630dc2c1a4e500236a85481
Matthew Warner <mattwarner14@yahoo.com>
2018-07-06 14:49:01 -0600

    finished functionality with the exception of a couple soon to be found bugs

--- src/components/schedule/scheduleCourse.js
+++ src/components/schedule/scheduleCourse.js
@@ -5,14 +5,14 @@ import * as actions from '../../actions';
 import Action from '../action';
 
 class ScheduleCourse extends Component {
-    render() {
-      return (
-        <div className="schedule-course">
-            <label className="schedule-course__label">{this.props.title}</label>
-            <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="schedule-course__action"/>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div className="schedule-course">
+        <label className="schedule-course__label">{this.props.title}</label>
+        <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="schedule-course__action"/>
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(ScheduleCourse);
