c3f54534132c72185cb45e7801d29d982ab44bdc
Matthew Warner <mattwarner14@yahoo.com>
2018-07-06 09:17:01 -0600

    redid github repo

---
+++ src/components/library/libraryCourse.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+import Icon from "../icon";
+import Arrow from "../arrow";
+import Action from "../action";
+
+class LibraryCourse extends Component {
+  render() {
+    return (
+      <div className="library-course">
+        <div className="library-course__title-check">
+          <label className="library-course__title">{ this.props.title }</label>
+          {Icon("fas fa-check", "library-course__icon")}
+        </div>
+        <Arrow className="library-course__arrow" />
+        <Action className="library-course__action"/>
+        <div className="library-course__description">
+          <label>Course Description</label>
+          <p>
+            { this.props.discription }
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LibraryCourse;
