32ad705f3e9be0dc0c07b98253723e6380b75b4f
Matthew Warner <mattwarner14@yahoo.com>
2018-07-20 10:59:36 -0600

    added course selected styles

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -13,19 +13,18 @@ class Arrow extends Component {
     toggleArrow = function() {
         this.props.callback(this.state.status)
         if(this.state.status) {
-            document.getElementById('this.id').classList.remove('arrow-closed');
-            
+            document.getElementById(this.id).classList.remove('arrow-closed');
         } else {
-            document.getElementById('this.id').classList.add('arrow-closed');
+            document.getElementById(this.id).classList.add('arrow-closed');
         }
 
         this.setState({ status: !this.state.status })
     }.bind(this);
-    
+
     render() {
-        this.id = `arrow ${this.props.id}`
+        this.id = `arrow-${this.props.id}`
         return (
-            <a id="arrow" onClick={() => this.toggleArrow()}className={`${this.props.className} arrow`}></a>
+            <a id={this.id} onClick={() => this.toggleArrow()} className={`${this.props.className} arrow`}></a>
         )
     }
 }
--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import { connect } from "react-redux";
+import * as actions from "../../actions";
 
 import Icon from "../icon";
 import Arrow from "../arrow";
@@ -16,39 +16,44 @@ class LibraryCourse extends Component {
     }
   }
 
-  renderDiscription = function() {
+  renderDescription = function() {
     if (!this.state.status) {
       return (
         <div className="library-course__description">
           <label>Course Description</label>
-          <p>{this.props.description }</p>
-      </div>
+          <p>{this.props.description}</p>
+        </div>
       )
     }
   }.bind(this);
 
   handleCallback = function(status) {
+    if(!status) {
+      document.getElementById('library-course').classList.add('library-course-selected');
+    } else {
+      document.getElementById('library-course').classList.remove('library-course-selected');
+    }
     this.setState({ status })
   }.bind(this)
 
   render() {
     return (
-      <div className="library-course">
+      <div id="library-course" className="library-course">
         <div className="library-course__title-check">
-          <label className="library-course__title">{ this.props.title }</label>
+          <label className="library-course__title">{this.props.title}</label>
           {Icon("fas fa-check", "library-course__icon")}
         </div>
-        <div className="library-course__line"></div>
-        <Arrow 
-          callback={status => this.handleCallback(status)} 
+        <div className="library-course__line" />
+        <Arrow
+          callback={status => this.handleCallback(status)}
           id={this.props.id}
-          className="library-course__arrow" 
-          />
-        <Action 
-          onClick={() => this.props.toggleEnrolled(this.props.id)} 
+          className="library-course__arrow"
+        />
+        <Action
+          onClick={() => this.props.toggleEnrolled(this.props.id)}
           className="library-course__action"
-          />
-          {this.renderDiscription()}
+        />
+        {this.renderDescription()}
       </div>
     );
   }
